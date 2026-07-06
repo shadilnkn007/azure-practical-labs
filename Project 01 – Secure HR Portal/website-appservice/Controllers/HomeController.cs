af8dbeef4b50b6a5ed6a546b41a5706468880f88
shadilnkn007 <54212594+shadilnkn007@users.noreply.github.com>
2026-07-06 23:58:53 +0530

    Update HomeController.cs

--- Project 01 – Secure HR Portal/website-appservice/Controllers/HomeController.cs
+++ Project 01 – Secure HR Portal/website-appservice/Controllers/HomeController.cs
@@ -4,11 +4,11 @@ namespace HRPortalApp.Controllers
 {
     public class HomeController : Controller
     {
-        private string storageAccount = "<your-storage-account>";
+        private string storageAccount = "hrportalstrg";
         private string container = "hr-documents";
 
         // SAS token placeholder
-        private string sasToken = "<your-sas-token>";
+        private string sasToken = "sp=r&st=2026-07-06T18:13:21Z&se=2026-07-07T02:28:21Z&spr=https&sv=2026-02-06&sr=c&sig=Eep0AUwVXHcVr3SZZt9jMiyhKOuAsnXpdWwjwGFnV9s%3D";
 
         public IActionResult Index()
         {
